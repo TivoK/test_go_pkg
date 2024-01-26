2278f2171c799fe7910a0bf02452e19f2a7089a0
tivo_k <tivo.kenevil@gmail.com>
2024-01-25 23:19:06 -0600

    change go.mod

--- test.go
+++ test.go
@@ -1,20 +1,15 @@
-package main 
+package main
 
 import "fmt"
 
-func Bark(){
-	
-	return fmt.Println("bark!")
-	
+func Bark() {
+
+	fmt.Println("bark!")
+
 }
 
-func main(){
-	
+func main() {
+
 	Bark()
 
 }
-
-
-
-
-
